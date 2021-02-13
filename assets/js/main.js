b7810b9797ad44513720318ea3f6ff237b77f929
Ayesha Muhammad Anabia <you@example.com>
2021-02-13 19:56:59 +0300

    start pemodule

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.querySelector('.toggle');
-const headerArea = document.querySelector('.header-area');
-
-menuToggle.addEventListener('click', () => {
-    menuToggle.classList.toggle('active')
-    headerArea.classList.toggle('active')
-})
